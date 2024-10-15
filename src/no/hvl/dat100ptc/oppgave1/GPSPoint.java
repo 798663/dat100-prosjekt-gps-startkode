9b4c4bc18414ef99f7d8a2e6380cabcf00f56c12
Andy25000 <asgarfors@gmail.com>
2024-10-15 13:12:02 +0200

    Update GPSPoint.java

--- src/no/hvl/dat100ptc/oppgave1/GPSPoint.java
+++ src/no/hvl/dat100ptc/oppgave1/GPSPoint.java
@@ -19,56 +19,40 @@ public class GPSPoint {
 	}
 
 	public int getTime() {
-		
 		return time;
 	}
 
 	public void setTime(int time) {
-     
 		this.time = time;
 	}
 
 	public double getLatitude() {
-		
-	  return latitude;
-		
-		}
+	  	return latitude;
+	}
 
 	public void setLatitude(double latitude) {
-		
      	this.latitude = latitude;
-		
 	}
 
 	public double getLongitude() {
-		
 		return longitude;
-		
 	}
 
 	public void setLongitude(double longitude) {
-		
 		this.longitude = longitude;
-		
 	}
 
 	public double getElevation() {
-		
 		return elevation;
-		
 	}
 
 	public void setElevation(double elevation) {
-		
-this.elevation = elevation;
-		
+		this.elevation = elevation;
 	}
 	
 	public String toString() {
-		
 		String str;
-	str = time + " (" + latitude + "," + longitude + ") " + elevation +"\n";
-			return str;
-		
+		str = time + " (" + latitude + "," + longitude + ") " + elevation +"\n";
+		return str;
 	}
 }
