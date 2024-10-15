f54ee99c29f6167f47962a02e6dfc858f752192f
798663 <798663@stud.hvl.no>
2024-10-15 17:32:20 +0100

    2

--- src/no/hvl/dat100ptc/oppgave2/GPSData.java
+++ src/no/hvl/dat100ptc/oppgave2/GPSData.java
@@ -8,41 +8,61 @@ public class GPSData {
 	private GPSPoint[] gpspoints;
 	protected int antall = 0;
 
-	public GPSData(int antall) {
-
+	
+	
+	public GPSData(int n) {
+        gpspoints = new GPSPoint[n];
+		this.antall = 0;
 		
-		
-		// TODO
 	}
 
+	
+
+	
 	public GPSPoint[] getGPSPoints() {
-		return this.gpspoints;
+	    return this.gpspoints;
 	}
+	
+	
+
 	
 	protected boolean insertGPS(GPSPoint gpspoint) {
+      	
+    if (antall < gpspoints.length) {
+        		gpspoints[antall] = gpspoint;
+        		antall++;
+        	
+        return true; 
+         }
+    else {
+	    return false;
+         }
+}
 
-		boolean inserted = false;
-		
-		throw new UnsupportedOperationException(TODO.method());
-		
-		// TODO 
 	
-	}
+	
 
 	public boolean insert(String time, String latitude, String longitude, String elevation) {
 
-		GPSPoint gpspoint;
-
-		throw new UnsupportedOperationException(TODO.method());
-
-		// TODO 
+		int time1 = Integer.parseInt(time);
+		double latitude1 = Double.parseDouble(latitude);
+		double longitude1 = Double.parseDouble(longitude);
+		double elevation1 = Double.parseDouble(elevation);
+	
+		 GPSPoint nyttGPSPunkt = new GPSPoint(time1, latitude1, longitude1, elevation1);
+		 
+		 return insertGPS(nyttGPSPunkt);
 		
 	}
 
 	public void print() {
 
-		throw new UnsupportedOperationException(TODO.method());
-
-		// TODO 
+		System.out.println("====== GPS Data - START ======");
+		for (int i = 0; i < antall; i++) {
+			System.out.println((i + 1) + " " + gpspoints[i].toString());
+		}
+		
+		
+		System.out.println("====== GPS Data - SLUTT ======");
 	}
 }
--- src/no/hvl/dat100ptc/oppgave2/Main.java
+++ src/no/hvl/dat100ptc/oppgave2/Main.java
@@ -4,10 +4,19 @@ import no.hvl.dat100ptc.oppgave1.GPSPoint;
 
 public class Main {
 
-	
 	public static void main(String[] args) {
-
-		// TODO
+        
+		GPSPoint gpspoint1 = new GPSPoint(35000, 387990, 5.255000, 60);
+	    GPSPoint gpspoint2 = new GPSPoint(40000, 400000, 5.280000, 70);
+	
+		GPSData gpsData = new GPSData(2);
+		
+		
+		gpsData.insertGPS(gpspoint1);
+		gpsData.insertGPS(gpspoint2);
+		
+		
+		gpsData.print();
 		
 	}
 }
