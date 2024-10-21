51bd9b8e7d79c025f821ff7045348aa19a4db79a
Andy25000 <asgarfors@gmail.com>
2024-10-21 16:13:18 +0200

    7

--- src/no/hvl/dat100ptc/oppgave2/Main.java
+++ src/no/hvl/dat100ptc/oppgave2/Main.java
@@ -10,9 +10,10 @@ public class Main {
 		//GPSPoint gpspoint1 = new GPSPoint(35000, 387990, 5.255000, 60);
 	    GPSPoint gpspoint2 = new GPSPoint(40000, 400000, 5.280000, 70);
 	
-		GPSData gpsData = new GPSData(3);
+		GPSData gpsData = new GPSData(2);
 
 		GPSPoint gpspoint = GPSDataConverter.convert("2017-08-13T08:52:26.000Z","60.385390","5.217217","61.9");
+
 		
         System.out.println("GPS-punktet er: " + gpspoint.toString());
 
