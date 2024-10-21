776afd3e705d4213a80e3912091425cc3703c557
Andy25000 <asgarfors@gmail.com>
2024-10-21 16:12:02 +0200

    8

--- src/no/hvl/dat100ptc/oppgave1/GPSPoint.java
+++ src/no/hvl/dat100ptc/oppgave1/GPSPoint.java
@@ -57,6 +57,7 @@ public class GPSPoint {
 		this.elevation = elevation;
 	}
 	
+        @SuppressWarnings("override")
 	public String toString() {
 		
 		String str;
--- src/no/hvl/dat100ptc/oppgave2/Main.java
+++ src/no/hvl/dat100ptc/oppgave2/Main.java
@@ -7,19 +7,18 @@ public class Main {
 
 	public static void main(String[] args) {
         
-		GPSPoint gpspoint1 = new GPSPoint(35000, 387990, 5.255000, 60);
+		//GPSPoint gpspoint1 = new GPSPoint(35000, 387990, 5.255000, 60);
 	    GPSPoint gpspoint2 = new GPSPoint(40000, 400000, 5.280000, 70);
 	
-		GPSData gpsData = new GPSData(2);
+		GPSData gpsData = new GPSData(3);
 
 		GPSPoint gpspoint = GPSDataConverter.convert("2017-08-13T08:52:26.000Z","60.385390","5.217217","61.9");
-
-        // Skriv ut GPS-punktet
+		
         System.out.println("GPS-punktet er: " + gpspoint.toString());
 
 		
 
-		gpsData.insertGPS(gpspoint1);
+		gpsData.insertGPS(gpspoint);
 		gpsData.insertGPS(gpspoint2);
 
 		
